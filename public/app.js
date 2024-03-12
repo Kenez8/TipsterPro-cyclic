0163161beaf6850da5c3fb581adf3755ec41259a
Kenez8 <152496964+Kenez8@users.noreply.github.com>
2024-03-12 22:56:39 +0100

    Update app.js odds

--- public/app.js
+++ public/app.js
@@ -191,7 +191,7 @@ document.addEventListener('DOMContentLoaded', function() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ sport, team1, team2, tip, bet, message }),
+            body: JSON.stringify({ sport, team1, team2, tip, bet, odds, message }),
         })
         .then(response => response.json())
         .then(data => {
